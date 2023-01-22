2d614f9a5aea5540531ffc673eb09822b6e7bb2f
Stefan Quach <stefantquach@gmail.com>
2023-01-21 20:15:20 -0800

    Fixing schematics and firmware

--- firmware/rules.mk
+++ firmware/rules.mk
@@ -1,10 +1,20 @@
+
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no            # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes
+
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = IS31FL3733
-
-RGBLIGHT_ENABLE = no
-# RGBLIGHT_DRIVER = WS2812
 
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306
 
+ENCODER_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
+
+WPM_ENABLE = yes
